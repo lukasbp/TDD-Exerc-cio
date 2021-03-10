0b97a57e889f7b43908cc9259a81876a8913b58e
Lucas Belinelli Pereira <lucas.pereira@ykt.com.br>
2021-03-10 17:53:07 -0300

    Criação previa de todos os objetos que serão usados para os testes

--- test/gerador.test.js
+++ test/gerador.test.js
@@ -1,3 +1,41 @@
 const Gerador = require('../lib/gerador');
 const Objeto = require('validate');
 const { toBeDeepCloseTo, ToMatchCloseTo } = require('jest-matcher-deep-close-to');
+
+expect.extend({ toBeDeepCloseTo, ToMatchCloseTo });
+
+const Gerador = new Gerador();
+
+const Cliente = new Objeto({
+    Nome: String,
+    Salario: Number,
+    Idade: Number,
+    valorDoEmprestimo: Number
+})
+
+describe('Teste geral',()=>{
+    test('até R$ 1000', ()=>{
+        const Cliente1 = {
+            Nome: 'Lucas',
+            Salario: 600,
+            Idade: 20,
+            valorDoEmprestimo: 900
+        }
+    })
+    test('de R$ 1000,1 até R$ 5000', ()=>{
+        const Cliente1 = {
+            Nome: 'João',
+            Salario: 2000,
+            Idade: 30,
+            valorDoEmprestimo: 4500
+        }
+    })
+    test('de R$ 5000 para cima', ()=>{
+        const Cliente1 = {
+            Nome: 'Matias',
+            Salario: 10000,
+            Idade: 45,
+            valorDoEmprestimo: 25000
+        }
+    })
+})
