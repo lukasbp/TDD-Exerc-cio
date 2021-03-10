c756869674f0451d849d5f3d73a854addcde8778
Lucas Belinelli Pereira <lucas.pereira@ykt.com.br>
2021-03-10 18:01:51 -0300

    criação da validação de caso o cliente exista

--- test/gerador.test.js
+++ test/gerador.test.js
@@ -13,29 +13,50 @@ const Cliente = new Objeto({
     valorDoEmprestimo: Number
 })
 
-describe('Teste geral',()=>{
-    test('até R$ 1000', ()=>{
-        const Cliente1 = {
+describe('Teste geral', () => {
+    test('até R$ 1000', () => {
+        const Cliente = {
             Nome: 'Lucas',
             Salario: 600,
             Idade: 20,
             valorDoEmprestimo: 900
         }
+
+        const Invalido = Gerador.validate(Cliente)
+
+        expect(Invalido).toHaveLength
+
+        const { Faixa, Total, Emprestimo } = gerador.GerarProposta(Cliente)
+
     })
-    test('de R$ 1000,1 até R$ 5000', ()=>{
-        const Cliente1 = {
+    test('de R$ 1000,1 até R$ 5000', () => {
+        const Cliente = {
             Nome: 'João',
             Salario: 2000,
             Idade: 30,
             valorDoEmprestimo: 4500
         }
+
+        const Invalido = Gerador.validate(Cliente)
+
+        expect(Invalido).toHaveLength
+
+        const { Faixa, Total, Emprestimo } = gerador.GerarProposta(Cliente)
+
     })
-    test('de R$ 5000 para cima', ()=>{
-        const Cliente1 = {
+    test('de R$ 5000 para cima', () => {
+        const Cliente = {
             Nome: 'Matias',
             Salario: 10000,
             Idade: 45,
             valorDoEmprestimo: 25000
         }
+        
+        const Invalido = Gerador.validate(Cliente)
+
+        expect(Invalido).toHaveLength
+
+        const { Faixa, Total, Emprestimo } = gerador.GerarProposta(Cliente)
+
     })
 })
